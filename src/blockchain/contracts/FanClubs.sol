c303bb78df254f7cb9d3d8d44522ecdd606ec51f
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-07-12 20:56:24 +0200

    feat: final setup in blockchain area

--- src/blockchain/contracts/FanClubs.sol
+++ src/blockchain/contracts/FanClubs.sol
@@ -26,6 +26,17 @@ contract FanClubs {
     mapping(string => mapping(address => uint256[])) private fanClubNFTs;
     mapping(string => mapping(address => uint256)) private fanTokenBalances; 
 
+    struct MarketplaceItem {
+        address nftAddress;
+        uint256 tokenId;
+        uint256 price;
+        bool isListed;
+    }
+
+    mapping(string => address) private fanToken;
+    mapping(string => MarketplaceItem[]) private marketplaceListings;
+    mapping(string => mapping(address => mapping(uint256 => bool))) private isNFTListed;
+
     modifier onlyClubOwner(string memory fanClubId) {
         require(fanClubExists[fanClubId], "Fan club does not exist");
         require(msg.sender == fanClubs[fanClubId].owner, "Only fan club owner");
@@ -33,6 +44,8 @@ contract FanClubs {
     }
 
     constructor() {}
+
+    // Fan club functions
 
     function createFanClub(string memory fanClubId, uint256 _price) external {
         require(!fanClubExists[fanClubId], "Fan club already exists");
@@ -117,6 +130,8 @@ contract FanClubs {
         return fanClubBalances[fanClubId];
     }
 
+    // Fan token functions
+
     function depositFanTokens(string memory fanClubId, address tokenAddress, uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
 
@@ -147,6 +162,8 @@ contract FanClubs {
     function getFanTokenBalance(string memory fanClubId, address tokenAddress) external onlyClubOwner(fanClubId) view returns (uint256) {
         return fanTokenBalances[fanClubId][tokenAddress];
     }
+
+    // Fan NFT functions
 
     function depositFanNFT(string memory fanClubId, address nftAddress, uint256 tokenId) external {
         require(fanClubExists[fanClubId], "Fan club does not exist");
@@ -203,7 +220,7 @@ contract FanClubs {
         IERC721(nftAddress).transferFrom(address(this), recipient, tokenId);
     }
 
-    function getFanNFT(string memory fanClubId, address nftAddress, uint256 tokenId) external view returns (bool) {
+    function getFanNFT(string memory fanClubId, address nftAddress, uint256 tokenId) public view returns (bool) {
         require(fanClubExists[fanClubId], "Fan club does not exist");
 
         uint256[] storage tokenList = fanClubNFTs[fanClubId][nftAddress];
@@ -217,5 +234,93 @@ contract FanClubs {
         }
 
         return found;
+    }
+
+    // Marketplace functions
+
+    function createMarketplace(string memory fanClubId, address tokenAddress) external onlyClubOwner(fanClubId) {
+        require(fanClubExists[fanClubId], "Fan club does not exist");
+        require(tokenAddress != address(0), "Invalid token address");
+        fanToken[fanClubId] = tokenAddress;
+    }
+
+    function listItem(string memory fanClubId, address nftAddress, uint256 tokenId, uint256 price) external onlyClubOwner(fanClubId) {
+        require(price > 0, "Price must be greater than 0");
+        require(getFanNFT(fanClubId, nftAddress, tokenId), "NFT not held by fan club");
+        require(!isNFTListed[fanClubId][nftAddress][tokenId], "Already listed");
+
+        MarketplaceItem memory newItem = MarketplaceItem({
+            nftAddress: nftAddress,
+            tokenId: tokenId,
+            price: price,
+            isListed: true
+        });
+
+        marketplaceListings[fanClubId].push(newItem);
+        isNFTListed[fanClubId][nftAddress][tokenId] = true;
+    }
+
+    function delistItem(string memory fanClubId, address nftAddress, uint256 tokenId) external onlyClubOwner(fanClubId) {
+        uint256 len = marketplaceListings[fanClubId].length;
+
+        for (uint256 i = 0; i < len; i++) {
+            if (
+                marketplaceListings[fanClubId][i].nftAddress == nftAddress &&
+                marketplaceListings[fanClubId][i].tokenId == tokenId &&
+                marketplaceListings[fanClubId][i].isListed
+            ) {
+                marketplaceListings[fanClubId][i].isListed = false;
+                isNFTListed[fanClubId][nftAddress][tokenId] = false;
+                break;
+            }
+        }
+    }
+
+    function buy(string memory fanClubId, address nftAddress, uint256 tokenId) external {
+        require(fanClubExists[fanClubId], "Fan club does not exist");
+        require(fanClubs[fanClubId].isMember[msg.sender], "Only members can buy");
+        require(isNFTListed[fanClubId][nftAddress][tokenId], "Item not listed");
+
+        address token = fanToken[fanClubId];
+        require(token != address(0), "Marketplace not initialized");
+
+        MarketplaceItem[] storage items = marketplaceListings[fanClubId];
+        uint256 index = type(uint256).max;
+        uint256 price;
+
+        for (uint256 i = 0; i < items.length; i++) {
+            if (
+                items[i].nftAddress == nftAddress &&
+                items[i].tokenId == tokenId &&
+                items[i].isListed
+            ) {
+                index = i;
+                price = items[i].price;
+                break;
+            }
+        }
+
+        require(index != type(uint256).max, "Item not found");
+
+        bool success = IERC20(token).transferFrom(msg.sender, fanClubs[fanClubId].owner, price);
+        require(success, "Payment failed");
+
+        IERC721(nftAddress).transferFrom(address(this), msg.sender, tokenId);
+
+        items[index].isListed = false;
+        isNFTListed[fanClubId][nftAddress][tokenId] = false;
+
+        uint256[] storage tokenList = fanClubNFTs[fanClubId][nftAddress];
+        for (uint256 i = 0; i < tokenList.length; i++) {
+            if (tokenList[i] == tokenId) {
+                tokenList[i] = tokenList[tokenList.length - 1];
+                tokenList.pop();
+                break;
+            }
+        }
+    }
+
+    function getItems(string memory fanClubId) external view returns (MarketplaceItem[] memory) {
+        return marketplaceListings[fanClubId];
     }
 }
